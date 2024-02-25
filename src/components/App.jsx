4c4607ea1098c8ae7483a117f6a06de7fddc6c0e
VolodymyrPopov <smartappsvp@gmail.com>
2024-02-25 20:49:40 +0000

    add local storage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,14 +5,26 @@ import Filter from './Filter/Filter';
 
 export class App extends Component {
   state = {
-    contacts: [
+    contacts: [],
+    filter: '',
+  };
+
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = contacts ? JSON.parse(contacts) : [
       {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
       {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
       {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
       {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
-    ],
-    filter: '',
-  };
+    ];
+    this.setState({ contacts: parsedContacts });
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
 
   addContact = (contact) => {
     this.setState(prevState => ({
